ed8437eed1ffd5cf2785b9036bc9612f03d3c6d5
fdelu <fdeluca@fi.uba.ar>
2022-11-09 19:12:12 -0300

    Move socket creation to SocketStatus

--- server/src/network/status.rs
+++ server/src/network/status.rs
@@ -1,14 +1,18 @@
 use std::net::SocketAddr;
 
 use actix::{dev::ToEnvelope, Actor, Addr, Handler};
-use tokio::task::{spawn, JoinHandle};
+use actix_rt::net::TcpStream;
+use tokio::{
+    task::{spawn, JoinHandle},
+    time::Duration,
+};
 
-use super::socket::{Socket, SocketEnd};
+use super::socket::{Socket, SocketEnd, SocketReceived};
 
 pub struct SocketStatus<A>
 where
-    A: Actor + Handler<SocketEnd>,
-    A::Context: ToEnvelope<A, SocketEnd>,
+    A: Actor + Handler<SocketEnd> + Handler<SocketReceived>,
+    A::Context: ToEnvelope<A, SocketEnd> + ToEnvelope<A, SocketReceived>,
 {
     socket: Addr<Socket>,
     cancel_task: Option<JoinHandle<()>>,
@@ -16,34 +20,41 @@ where
     addr: SocketAddr,
 }
 
-const CANCEL_TIMEOUT_SECONDS: u64 = 120;
+const CANCEL_TIMEOUT: Duration = Duration::from_secs(120);
 
 impl<A> SocketStatus<A>
 where
-    A: Actor + Handler<SocketEnd>,
-    A::Context: ToEnvelope<A, SocketEnd>,
+    A: Actor + Handler<SocketEnd> + Handler<SocketReceived>,
+    A::Context: ToEnvelope<A, SocketEnd> + ToEnvelope<A, SocketReceived>,
 {
-    pub fn new(socket: Addr<Socket>, cancel_addr: Addr<A>, addr: SocketAddr) -> Self {
-        Self {
+    pub fn new(
+        callback_actor: Addr<A>,
+        socket_addr: SocketAddr,
+        stream: Option<TcpStream>,
+    ) -> SocketStatus<A> {
+        let socket = Socket::new(callback_actor.clone(), socket_addr, stream).start();
+        let mut this = SocketStatus {
             socket,
             cancel_task: None,
-            cancel_actor: cancel_addr,
-            addr,
-        }
+            cancel_actor: callback_actor,
+            addr: socket_addr,
+        };
+        this.restart_timeout();
+        this
     }
 
-    pub fn cancel_task(&mut self) {
+    pub fn cancel_timeout(&mut self) {
         if let Some(task) = self.cancel_task.take() {
             task.abort();
         }
     }
 
-    pub fn restart_task(&mut self) {
-        self.cancel_task();
+    pub fn restart_timeout(&mut self) {
+        self.cancel_timeout();
         let actor = self.cancel_actor.clone();
         let addr = self.addr;
         self.cancel_task = Some(spawn(async move {
-            tokio::time::sleep(tokio::time::Duration::from_secs(CANCEL_TIMEOUT_SECONDS)).await;
+            tokio::time::sleep(CANCEL_TIMEOUT).await;
             actor.do_send(SocketEnd { addr });
         }));
     }
