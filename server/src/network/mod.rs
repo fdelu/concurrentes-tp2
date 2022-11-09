add21bb32605189f1b062504dd73d5e52edfec67
fdelu <fdeluca@fi.uba.ar>
2022-11-09 16:58:29 -0300

    Remove ConnectionHandler wrapper

--- server/src/network/mod.rs
+++ server/src/network/mod.rs
@@ -1,86 +1,165 @@
-use std::cmp::min;
-use std::io;
-use std::net::SocketAddr;
+use std::{collections::HashMap, mem::replace, net::SocketAddr};
 
-use actix::Actor;
-use actix::Addr;
-use actix_rt::task::JoinHandle;
-use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver};
+use actix::{
+    Actor, ActorFutureExt, Addr, AsyncContext, Context, Handler, ResponseActFuture, WrapFuture,
+};
+use actix_rt::{net::TcpStream, task::JoinHandle};
+use tokio::{
+    net::ToSocketAddrs,
+    sync::{
+        mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender},
+        oneshot,
+    },
+};
 
-use self::connections::ConnectionHandler;
-use self::listener::{Listener, OnConnection};
-use self::messages::{AddStream, SendPacket};
-use self::socket::SocketReceived;
-use self::socket::MAX_MESSAGE_SIZE;
-
-mod connections;
 mod error;
 mod listener;
 mod messages;
 mod socket;
 mod status;
 
-#[allow(dead_code)]
-pub struct ConnectionlessTCP {
-    actor: Addr<ConnectionHandler>,
-    receiver: UnboundedReceiver<SocketReceived>,
-    addr: io::Result<SocketAddr>,
-    listener_handle: JoinHandle<()>,
+pub use self::messages::*;
+use self::{
+    error::SocketError,
+    listener::{Listener, OnConnection},
+    socket::{Socket, SocketEnd, SocketReceived, SocketSend},
+    status::SocketStatus,
+};
+
+pub struct ConnectionHandler {
+    connections: HashMap<SocketAddr, SocketStatus<Self>>,
+    incoming_buffer: UnboundedSender<RecvOutput>,
+    recv_queue: oneshot::Receiver<UnboundedReceiver<RecvOutput>>,
+    join_listener: JoinHandle<()>,
 }
 
-#[allow(dead_code)]
-impl ConnectionlessTCP {
+impl Actor for ConnectionHandler {
+    type Context = Context<Self>;
+
+    fn stopped(&mut self, ctx: &mut Self::Context) {
+        self.join_listener.abort();
+    }
+}
+
+impl ConnectionHandler {
+    fn initialize(
+        ctx: &mut Context<Self>,
+        listener: Listener,
+        incoming_buffer: UnboundedSender<RecvOutput>,
+        recv_queue: oneshot::Receiver<UnboundedReceiver<RecvOutput>>,
+    ) -> Self {
+        let actor_addr = ctx.address();
+        Self {
+            connections: HashMap::new(),
+            incoming_buffer,
+            recv_queue,
+            join_listener: listener.run(Self::on_connection(actor_addr)),
+        }
+    }
+
+    pub async fn bind<T: ToSocketAddrs>(address: T) -> Result<Addr<Self>, SocketError> {
+        let listener = Listener::bind(address).await?;
+        let (incoming_tx, incoming_rx) = unbounded_channel();
+        let (recv_tx, recv_rx) = oneshot::channel();
+        recv_tx.send(incoming_rx).map_err(|_| {
+            SocketError::new("Failed to send buffer receiver through initial oneshot channel")
+        })?;
+
+        Ok(Self::create(move |ctx| {
+            Self::initialize(ctx, listener, incoming_tx, recv_rx)
+        }))
+    }
+
     fn on_connection(actor: Addr<ConnectionHandler>) -> OnConnection {
         Box::new(move |stream, addr| actor.do_send(AddStream { addr, stream }))
     }
 
-    pub async fn send_to(&self, buf: &[u8], addr: SocketAddr) -> io::Result<usize> {
-        self.actor
-            .send(SendPacket {
-                to: addr,
-                data: buf[..MAX_MESSAGE_SIZE].to_vec(),
-            })
-            .await
-            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))??;
-        Ok(min(buf.len(), MAX_MESSAGE_SIZE))
-    }
-
-    pub async fn recv_from(&mut self, buf: &mut [u8]) -> io::Result<(usize, SocketAddr)> {
-        let received = self.receiver.recv().await.ok_or_else(|| {
-            io::Error::new(
-                std::io::ErrorKind::Other,
-                "Error receiving: internal channel is closed",
-            )
-        })?;
-        buf[..received.data.len()].copy_from_slice(&received.data);
-        Ok((received.data.len(), received.addr))
-    }
-
-    pub async fn bind(addr: SocketAddr) -> io::Result<Self> {
-        let (sender, receiver) = unbounded_channel();
-        let actor = ConnectionHandler::new(sender).start();
-
-        let listener = Listener::bind(addr).await?;
-        let addr = listener.get_addr();
-
-        Ok(ConnectionlessTCP {
-            actor: actor.clone(),
-            receiver,
-            listener_handle: listener.run(Self::on_connection(actor)),
-            addr,
-        })
-    }
-
-    pub fn local_addr(&self) -> io::Result<SocketAddr> {
-        match self.addr {
-            Ok(addr) => Ok(addr),
-            Err(ref e) => Err(io::Error::new(e.kind(), e.to_string())),
-        }
+    fn create_socket(
+        this_actor: Addr<Self>,
+        addr: SocketAddr,
+        stream: Option<TcpStream>,
+    ) -> SocketStatus<Self> {
+        let this_actor_clone = this_actor.clone();
+        let socket = Socket::new(this_actor, addr, stream).start();
+        SocketStatus::new(socket, this_actor_clone, addr)
     }
 }
 
-impl Drop for ConnectionlessTCP {
-    fn drop(&mut self) {
-        self.listener_handle.abort();
+// Public messages
+
+impl Handler<SendPacket> for ConnectionHandler {
+    type Result = ResponseActFuture<Self, Result<(), SocketError>>;
+
+    fn handle(&mut self, msg: SendPacket, _ctx: &mut Context<Self>) -> Self::Result {
+        let this_actor = _ctx.address();
+        let socket_addr = msg.to;
+        let status = self
+            .connections
+            .entry(msg.to)
+            .or_insert_with(move || Self::create_socket(this_actor, socket_addr, None));
+
+        status.restart_task();
+        let socket = status.get_socket();
+        async move { socket.send(SocketSend { data: msg.data }).await? }
+            .into_actor(self)
+            .boxed_local()
+    }
+}
+
+impl Handler<RecvPacket> for ConnectionHandler {
+    type Result = ResponseActFuture<Self, Result<RecvOutput, SocketError>>;
+
+    fn handle(&mut self, _msg: RecvPacket, _ctx: &mut Context<Self>) -> Self::Result {
+        let (tx, rx) = oneshot::channel();
+        let recv_queue = replace(&mut self.recv_queue, rx);
+
+        async move {
+            let mut receiver = recv_queue.await?;
+            let out = receiver
+                .recv()
+                .await
+                .ok_or_else(|| SocketError::new("Error receiving: internal channel is closed"));
+            tx.send(receiver).map_err(|_| {
+                SocketError::new("Failed to send buffer receiver through oneshot channel")
+            })?;
+            out
+        }
+        .into_actor(self)
+        .boxed_local()
+    }
+}
+
+// Private Messages
+
+impl Handler<AddStream> for ConnectionHandler {
+    type Result = ();
+
+    fn handle(&mut self, msg: AddStream, _ctx: &mut Self::Context) -> Self::Result {
+        let this_actor = _ctx.address();
+        self.connections.insert(
+            msg.addr,
+            Self::create_socket(this_actor, msg.addr, Some(msg.stream)),
+        );
+    }
+}
+
+impl Handler<SocketEnd> for ConnectionHandler {
+    type Result = ();
+
+    fn handle(&mut self, msg: SocketEnd, _ctx: &mut Self::Context) {
+        self.connections.remove(&msg.addr);
+    }
+}
+
+impl Handler<SocketReceived> for ConnectionHandler {
+    type Result = ();
+
+    fn handle(&mut self, msg: SocketReceived, _ctx: &mut Self::Context) {
+        if let Some(status) = self.connections.get_mut(&msg.addr) {
+            status.restart_task();
+        }
+        if let Err(e) = self.incoming_buffer.send((msg.addr, msg.data)) {
+            println!("Error sending through channel: {e}");
+        }
     }
 }
